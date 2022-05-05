992e254f4d68cb2705b419b4ec3284b8a774ca88
fullgon <mbj0119@gmail.com>
2022-05-05 22:44:16 +0900

    커밋연습

--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Invo from "./routes/invo"
-import Expen from "./routes/expen"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App/>} />
-      <Route path="invo" element={<Invo/>} />
-      <Route path="expen" element={<Expen/>} />
     </Routes>
   </BrowserRouter>,
   root
