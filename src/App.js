89331d6b2bcc74159d583692daf23d7fe275f28d
fullgon <mbj0119@gmail.com>
2022-05-05 18:59:50 +0900

    react-router-dom practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bookkeeper</h1>
+      <nav style={{borderBottom:"solid 1px", paddingBottom: "1rem",}}>
+        <Link to="/invo">invo</Link> |{" "}
+        <Link to="/expen">expen</Link>
+        
+      </nav>
     </div>
   );
 }
