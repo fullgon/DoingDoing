a37885ed9465d9f4583527cd3c51aa45dd74c936
fullgon <mbj0119@gmail.com>
2022-05-11 22:28:58 +0900

    axios 연습용 작성

--- src/mainToDo/component/ToDoDate.js
+++ src/mainToDo/component/ToDoDate.js
@@ -1,8 +1,26 @@
+import { Axios } from "axios";
+import { useEffect, useState } from "react";
 
 
 function ToDoDate(){
+
+    const [todo, setTodo] = useState("");
+
+    useEffect(()=>{
+        Axios.post("/api/date").then((response)=>{
+            if(response.data){
+                setTodo(response.data);
+            }
+            else{
+                alert("fail");
+            }
+        })
+    },[])
     return(
-        <div></div>
+        <div>
+            <h1>{setTodo.todo}</h1>
+            <h1>{setTodo.date}</h1>
+        </div>
     )
 }
 
