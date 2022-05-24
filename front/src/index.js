026d6a91a386b09c75ca9326a1d6ec1afdcb65be
fullgon <mbj0119@gmail.com>
2022-05-24 11:34:16 +0900

    package.json update

--- front/src/index.js
+++ front/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignIn from './sign/SignIn';
 import SignUp from './sign/SignUp';
+import FindPassword from './sign/FindPassword'
 import ToDoMain from './mainToDo/ToDoMain';
 import ReactModal from 'react-modal'
 
@@ -19,6 +20,7 @@ root.render(
       <Route path="/" element={<App/>} />
       <Route path="auth/signIn" element={<SignIn/>} />
       <Route path='auth/signUp' element={<SignUp/>}/>
+      <Route path='auth/findPassword' element={<FindPassword/>}/>
       <Route path='schedule' element={<ModalsProvider><ToDoMain/></ModalsProvider>}/>
     </Routes>
   </BrowserRouter>,
