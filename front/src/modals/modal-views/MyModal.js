f0f877ea15a5f36eb216f626e29541217347411a
fullgon <mbj0119@gmail.com>
2022-05-27 10:16:38 +0900

    MyModal을 사용한 Modal 재사용 기능 추가

--- front/src/modals/modal-views/MyModal.js
+++ front/src/modals/modal-views/MyModal.js
@@ -1,11 +1,19 @@
+import { useEffect, useState } from 'react';
 import ReactModal from 'react-modal'
+import DetailSchedule from './DtailSchedule';
 import NewSchedule from './NewSchedule';
 
-const MyModal = ({ onSubmit, onClose }) => {
-    
+const MyModal = ({ onSubmit, onClose, type }) => {
+    let Modal = null;
+    if(type == "New"){
+        Modal = NewSchedule;
+    }
+    else{
+        Modal = DetailSchedule;
+    }
     return(
         <ReactModal isOpen>
-            <NewSchedule onSubmit={onSubmit} onClose={onClose} />
+            <Modal onSubmit={onSubmit} onClose={onClose} />
         </ReactModal>
     );
 };
