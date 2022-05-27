f06612e783cfa229213cff06120b3e025f4be914
phj1120 <hyeonj1998@naver.com>
2022-05-27 23:36:39 +0900

    Delete Backend_connect_test

--- Backend_connect_test/src/main/java/xyz/parkh/campus/repository/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package xyz.parkh.campus.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import xyz.parkh.campus.entity.TestEntity;
-
-@Repository
-public interface TestRepository extends JpaRepository<TestEntity, Integer> {
-}
--- Backend_connect_test/src/main/java/xyz/parkh/campus/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package xyz.parkh.campus.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import xyz.parkh.campus.entity.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-    User findByUserId(String userId);
-    Boolean existsByUserId(String UserId);
-}
