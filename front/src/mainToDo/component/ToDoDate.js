9b8f91e16c01d26d82aeb1e520e390d7217f0397
fullgon <mbj0119@gmail.com>
2022-05-31 22:43:05 +0900

    ADD: ModifySchedule 생성

--- front/src/mainToDo/component/ToDoDate.js
+++ front/src/mainToDo/component/ToDoDate.js
@@ -7,23 +7,11 @@ function ToDoDate(){
 
     const [todo, setTodo] = useState("");
   
-
-    const action = () =>{
-        axios.post("/api/account/signUp", {
-            userId:"ad",
-            password:"aa"
-        }).then((response)=>{
-            if(response.data){
-                setTodo(response.data.userId);
-            }
-        }).catch(error =>{
-            alert("fail");
-        })
-    }
+    
     return(
         <div className={`${styles.item} ${styles.body}`}>
             <h1>할 일(Limit){todo}</h1>
-            <button onClick={action}>프록시 연결</button>
+            
             
             
         </div>
---
+++ front/src/modals/modal-views/ModifySchedule.js
@@ -0,0 +1,7 @@
+
+
+const ModifySchedule = () =>{
+
+}
+
+export default ModifySchedule;
