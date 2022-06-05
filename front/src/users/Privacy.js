ce9a6beaa8241c27e115868723ae1cf5f424835a
fullgon <mbj0119@gmail.com>
2022-06-05 21:52:39 +0900

    ADD:개인정보와 비밀번호 변경 선택적으로 보이기 및 비밀번호 변경에 state변경 함수 전달

--- front/src/users/Privacy.js
+++ front/src/users/Privacy.js
@@ -1,7 +1,32 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import ChangePassword from "./ChangePassword";
+import UserInfo from "./UserInfo";
 
 
 const Privacy = () =>{
-    
+
+    const [isPasswordChange, setIsPasswordChange] = useState(false);
+
+    const changePassword = () => {
+        setIsPasswordChange(false);
+    }
+
+    return(
+        <div>
+           {isPasswordChange? 
+           <ChangePassword setIsPasswordChange={changePassword}/> :
+           <div>
+                <UserInfo/>
+                <div onClick={() => setIsPasswordChange(true)}>비밀번호 변경</div>
+                <div>회원탈퇴</div>   
+            </div>}
+            
+            
+            
+        </div>
+    )
 }
 
 export default Privacy;
