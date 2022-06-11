78f7f3d70e58dd7b1b5b296166d3b0cf2a377afa
fullgon <mbj0119@gmail.com>
2022-06-11 21:39:00 +0900

    MOD: ToDoDate 일정 리스트 css변경

--- front/src/mainToDo/component/ToDoDate.js
+++ front/src/mainToDo/component/ToDoDate.js
@@ -80,8 +80,8 @@ function ToDoDate(){
                         <div className={styles.schedule}>
                             <div className={styles['check-point']}> </div>
                             <a className={styles.title} onClick={() => {handleClick(schedule.no)}}>{schedule.title}</a>
-                            <a className={styles.date} onClick={() => {handleClick(schedule.no)}}>{schedule.endDate}</a>
                         </div>
+                        <a className={styles.date} onClick={() => {handleClick(schedule.no)}}>{schedule.endDate}</a>
                         <input type="checkbox" className={styles.checkbox}
                         onChange={(event)=>{isCompleteSchedule(event.target.checked, schedule.no)}}/>
                     </div>
