7e0e51da5ca0b491362c1131d385941b4af4ff58
phj1120 <hyeonj1998@naver.com>
2022-07-25 12:27:29 +0900

    수정:User 엔티티 개발 -친구 관계

--- Backend_Renewal/src/main/java/xyz/parkh/doing/domain/entity/User.java
+++ Backend_Renewal/src/main/java/xyz/parkh/doing/domain/entity/User.java
@@ -4,8 +4,11 @@ import lombok.*;
 import xyz.parkh.doing.domain.model.Auth;
 import xyz.parkh.doing.domain.model.UserDetailInfo;
 import xyz.parkh.doing.domain.model.UserInfo;
+import xyz.parkh.doing.domain.model.friend.FriendshipState;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -33,6 +36,9 @@ public class User {
 
     @Column(length = 50)
     private String company;
+
+    @OneToMany(mappedBy = "id")
+    private List<Friendship> friendships = new ArrayList<>();
 
     @Builder
     public User(Long no, String userId, String password, String name, String email, String company) {
@@ -75,5 +81,39 @@ public class User {
             this.email = userInfo.getEmail();
         }
     }
+
+    /*
+    * 외래키가 Friendship 에 있기 때문에 객체 지향적인 것을 포기하더라도,
+    * Friendship 에서 친구를 관리하는 것이 좋지만,
+    * 조회 정도는 자주 할 것 같다고 판단해 양방향 매핑 추가.
+    * */
+     public List<User> readFriends() {
+        List<User> memberList = new ArrayList<>();
+        for (Friendship friendship : friendships) {
+            if (friendship.getFriendshipState().equals(FriendshipState.ACCEPT)) {
+                memberList.add(friendship.getRequester());
+            }
+        }
+        return memberList;
+    }
+
+    public List<User> readSentFriendRequests() {
+        List<User> memberList = new ArrayList<>();
+        for (Friendship friendship : friendships) {
+            if (friendship.getFriendshipState().equals(FriendshipState.REQUEST)) {
+                memberList.add(friendship.getRequester());
+            }
+        }
+        return memberList;
+    }
+
+// TODO
+//  가능 하다면 친구 요청 들어온 목록 확인 하는 메서드 생성
+//  불가능 할 경우 readSentFriendRequests 도 삭제
+//  JPQL 학습 후
+//  Repository 에서 조회 하는게 나을지
+//  여기서 조회 하는게 나을지 생각 해보기
+//    public List<User> readReceivedFriendRequests() {}
+
 }
 
