07e80a72bb30e8ab2e56faa82855ff4f8a3add00
fullgon <mbj0119@gmail.com>
2022-07-28 11:55:54 +0900

    ADD: Schedule_test파일 생성 및 렌더링

--- front/src/index.js
+++ front/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Schedule from './Schedule_test';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Schedule />
   </React.StrictMode>
 );
