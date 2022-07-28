cf4e63788ae0260d4aa3bc3cc8884e94037f5eff
fullgon <mbj0119@gmail.com>
2022-07-28 11:59:20 +0900

    ADD:ScheduleBanner파일 생성

---
+++ front/src/component/ScheduleBanner.js
@@ -0,0 +1,11 @@
+
+const ScheduleBanner = () =>{
+    return(
+        <div>
+            <div className="color-line"></div>
+            
+        </div>
+    )
+}
+
+export default ScheduleBanner;
