083d2975458bdff52fcf152910a50c4d7d4a2537
fullgon <mbj0119@gmail.com>
2022-08-29 20:58:45 +0900

    MOD: 일정에 주석 및 체크박스, 일정제목 추가

--- front/src/component/Schedule.js
+++ front/src/component/Schedule.js
@@ -1,10 +1,26 @@
 import styled from 'styled-components'
 
+//체크박스 + 일정제목 + 쓰레기통표시
+//체크박스 체크 시 일정제목 회색 및 가로줄
+//일정에 마우스 올릴 시 오른쪽 끝에 휴지통 아이콘 생성
+
+const Container = styled.div`
+display: flex;
+`;
+
+const CheckBox = styled.input.attrs(props => ({
+    type: "checkbox",
+}))`
+
+`;
+
 const Schedule = () =>{
     return(
-        <div>
-
-        </div>
+        <Container>
+            <CheckBox/>
+            안녕
+            <CheckBox/>
+        </Container>
     )
 }
 
