85c51ba84e190479c31c07569cac651e946a90eb
fullgon <mbj0119@gmail.com>
2022-09-05 21:34:30 +0900

    ADD: 각 스케줄에 mouseover 시 icon 비/활성화 기능 추가

--- front/src/component/Schedule.js
+++ front/src/component/Schedule.js
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import styled from 'styled-components'
 
 //체크박스 + 일정제목 + 쓰레기통표시
 //체크박스 체크 시 일정제목 회색 및 가로줄
 //일정에 마우스 올릴 시 오른쪽 끝에 휴지통 아이콘 생성
+
+//마우스 올렸을 때 실행할 아이콘 보이게 될 함수 생성
 
 const Container = styled.div`
 display: flex;
@@ -15,11 +18,18 @@ const CheckBox = styled.input.attrs(props => ({
 `;
 
 const Schedule = () =>{
+    const [visible, setVisible] = useState(false);
+    const visibleIcon = () =>{
+        setVisible(true);
+    }
+    const hiddenIcon = () =>{
+        setVisible(false);
+    }
     return(
-        <Container>
+        <Container onMouseOver={visibleIcon} onMouseOut={hiddenIcon}>
             <CheckBox/>
             안녕
-            <CheckBox/>
+            {visible? <CheckBox/> : null}
         </Container>
     )
 }
