2cfeee3f98e34842ff1510d9b9ed2113834798b6
fullgon <mbj0119@gmail.com>
2022-09-06 11:31:56 +0900

    MOD: 컨테이너 클릭 시 화살표 아이콘 변경하는 기능 추가

--- front/src/component/ToggleHistory.js
+++ front/src/component/ToggleHistory.js
@@ -1,10 +1,24 @@
+import { useState } from 'react';
+import styled from 'styled-components'
 
+const Container = styled.div`
+display: flex;
+`;
 
 const ToggleHistory = () => {
-    return(
-        <div>
 
-        </div>
+    const changeIcon = (e) =>{
+        if(e.target.innerText =="▶"){
+            e.target.innerText = "▼";
+            return
+        }
+        e.target.innerText = "▶";            
+    }
+    return(
+        <Container onClick={changeIcon}>
+            ▶
+            
+        </Container>
     )
 }
 
