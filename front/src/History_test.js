185b820bc06f7781d8e798033fb5ba195331530b
fullgon <mbj0119@gmail.com>
2022-09-06 11:21:02 +0900

    ADD: 일정기록을 보여줄 History_test컴포넌트 생성

---
+++ front/src/History_test.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+display: flex;
+`;
+
+
+const History_test = () => {
+    return (
+        <Container>
+
+        </Container>
+    )
+}
+
+export default History_test;
