785cfd1e5b18c738558599f9e2a99df7b0ad124a
fullgon <mbj0119@gmail.com>
2022-09-06 10:25:30 +0900

    MOD: 인풋박스에 포커스 아웃 또는 엔터 누를 시 api호출하며 텍스트 지우는 함수 추가

--- front/src/component/NewSchedule.js
+++ front/src/component/NewSchedule.js
@@ -1,10 +1,30 @@
+import styled from 'styled-components'
 
+const Container = styled.div`
+display: flex;
+`;
+
+const Input = styled.input.attrs(props => ({
+    type: "input",
+}))`
+
+`;
 
 const NewSchedule = () =>{
-    return (
-        <div>
 
-        </div>
+    const pressEnter = (e) =>{
+        if(e.key === 'Enter'){
+           addScheduleApiCall(e);
+        }
+    }
+
+    const addScheduleApiCall = (data) =>{
+       data.target.value = "";
+    };
+    return (
+        <Container>
+            <Input onBlur={addScheduleApiCall} onKeyPress={pressEnter}/>
+        </Container>
     )
 }
 
