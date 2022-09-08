f2dfc5e4ed9bedc2394126ea0d0d16e0d70475f8
phj1120 <hyeonj1998@naver.com>
2022-09-08 23:31:56 +0900

    수정 : @postconstruct 삭제

--- Backend_Renewal/src/test/java/xyz/parkh/doing/service/ScheduleServiceTest.java
+++ Backend_Renewal/src/test/java/xyz/parkh/doing/service/ScheduleServiceTest.java
@@ -18,6 +18,7 @@ import xyz.parkh.doing.domain.schedule.model.ScheduleDto;
 import xyz.parkh.doing.domain.schedule.repository.ScheduleRepository;
 import xyz.parkh.doing.domain.schedule.service.ScheduleService;
 import xyz.parkh.doing.domain.user.entity.User;
+import xyz.parkh.doing.domain.user.model.UserDetailInfo;
 import xyz.parkh.doing.domain.user.repository.UserRepository;
 import xyz.parkh.doing.domain.user.service.UserService;
 
@@ -42,6 +43,97 @@ public class ScheduleServiceTest {
 
     @Autowired
     FriendService friendService;
+
+
+    @Before
+    public void init() throws Exception {
+        if (userService.findByAuthId("authIdA") == null) {
+            UserDetailInfo userDetailInfoA = new UserDetailInfo("authIdA", "passwordA", "nameA", "emailA@naver.com", "companyA");
+            userService.signUp(userDetailInfoA);
+        }
+        if (userService.findByAuthId("authIdB") == null) {
+            UserDetailInfo userDetailInfoB = new UserDetailInfo("authIdB", "passwordB", "nameB", "emailB@naver.com", "companyB");
+            userService.signUp(userDetailInfoB);
+        }
+        if (userService.findByAuthId("authIdC") == null) {
+            UserDetailInfo userDetailInfoC = new UserDetailInfo("authIdC", "passwordC", "nameC", "emailC@naver.com", "companyC");
+            userService.signUp(userDetailInfoC);
+        }
+
+        User userA = userService.findByAuthId("authIdA");
+        User userB = userService.findByAuthId("authIdB");
+
+        friendService.requestFriendApplication(userA.getAuthId(), userB.getAuthId());
+        FriendInfo friendApplication = friendService.getReceiveFriendApplication(userA.getAuthId(), userB.getAuthId(), FriendshipState.REQUEST);
+        friendService.responseAccept(friendApplication.getId());
+
+        LocalDate now = LocalDate.now();
+        Period monthlyPeriod = Period.createMonthlyPeriod(now);
+        Period weeklyPeriod = Period.createWeeklyPeriod(now);
+        Period dailyPeriod = Period.createDailyPeriod(now);
+
+        if (scheduleService.findAllCategorizedScheduleList(now, userA.getAuthId(), userA.getAuthId()).getDaily().getHabitScheduleList().size() == 0) {
+            ScheduleDto monthToDoPublicTrue = ScheduleDto.createForToDoSchedule(userA, "MonthToDoPublicTrue", OpenScope.PUBLIC, monthlyPeriod, true);
+            ScheduleDto monthToDoPublicFalse = ScheduleDto.createForToDoSchedule(userA, "MonthToDoPublicFalse", OpenScope.PUBLIC, monthlyPeriod, false);
+            ScheduleDto monthHabitPublic = ScheduleDto.createForHabitSchedule(userA, "MonthHabitPublic", OpenScope.PUBLIC, monthlyPeriod);
+            ScheduleDto monthToDoPrivateTrue = ScheduleDto.createForToDoSchedule(userA, "MonthToDoPrivateTrue", OpenScope.PRIVATE, monthlyPeriod, true);
+            ScheduleDto monthToDoPrivateFalse = ScheduleDto.createForToDoSchedule(userA, "MonthToDoPublicTrue", OpenScope.PRIVATE, monthlyPeriod, false);
+            ScheduleDto monthHabitPrivate = ScheduleDto.createForHabitSchedule(userA, "MonthHabitPrivate", OpenScope.PRIVATE, monthlyPeriod);
+            ScheduleDto monthToDoFriendTrue = ScheduleDto.createForToDoSchedule(userA, "MonthToDoFriendTrue", OpenScope.FRIEND, monthlyPeriod, true);
+            ScheduleDto monthToDoFriendFalse = ScheduleDto.createForToDoSchedule(userA, "MonthToDoFriendTrue", OpenScope.FRIEND, monthlyPeriod, false);
+            ScheduleDto monthHabitFriend = ScheduleDto.createForHabitSchedule(userA, "MonthHabitFriend", OpenScope.FRIEND, monthlyPeriod);
+
+            scheduleService.addSchedule(monthToDoPublicTrue);
+            scheduleService.addSchedule(monthToDoPublicFalse);
+            scheduleService.addSchedule(monthHabitPublic);
+            scheduleService.addSchedule(monthToDoPrivateFalse);
+            scheduleService.addSchedule(monthToDoPrivateTrue);
+            scheduleService.addSchedule(monthHabitPrivate);
+            scheduleService.addSchedule(monthToDoFriendFalse);
+            scheduleService.addSchedule(monthToDoFriendTrue);
+            scheduleService.addSchedule(monthHabitFriend);
+
+            ScheduleDto weekToDoPublicTrue = ScheduleDto.createForToDoSchedule(userA, "weekToDoPublicTrue", OpenScope.PUBLIC, weeklyPeriod, true);
+            ScheduleDto weekToDoPublicFalse = ScheduleDto.createForToDoSchedule(userA, "weekToDoPublicFalse", OpenScope.PUBLIC, weeklyPeriod, false);
+            ScheduleDto weekHabitPublic = ScheduleDto.createForHabitSchedule(userA, "weekHabitPublic", OpenScope.PUBLIC, weeklyPeriod);
+            ScheduleDto weekToDoPrivateTrue = ScheduleDto.createForToDoSchedule(userA, "weekToDoPrivateTrue", OpenScope.PRIVATE, weeklyPeriod, true);
+            ScheduleDto weekToDoPrivateFalse = ScheduleDto.createForToDoSchedule(userA, "weekToDoPublicTrue", OpenScope.PRIVATE, weeklyPeriod, false);
+            ScheduleDto weekHabitPrivate = ScheduleDto.createForHabitSchedule(userA, "weekHabitPrivate", OpenScope.PRIVATE, weeklyPeriod);
+
+            ScheduleDto weekToDoFriendTrue = ScheduleDto.createForToDoSchedule(userA, "weekToDoFriendTrue", OpenScope.FRIEND, weeklyPeriod, true);
+            ScheduleDto weekToDoFriendFalse = ScheduleDto.createForToDoSchedule(userA, "weekToDoFriendTrue", OpenScope.FRIEND, weeklyPeriod, false);
+            ScheduleDto weekHabitFriend = ScheduleDto.createForHabitSchedule(userA, "weekHabitFriend", OpenScope.FRIEND, weeklyPeriod);
+            scheduleService.addSchedule(weekToDoPublicTrue);
+            scheduleService.addSchedule(weekToDoPublicFalse);
+            scheduleService.addSchedule(weekHabitPublic);
+            scheduleService.addSchedule(weekToDoPrivateTrue);
+            scheduleService.addSchedule(weekToDoPrivateFalse);
+            scheduleService.addSchedule(weekHabitPrivate);
+            scheduleService.addSchedule(weekToDoFriendTrue);
+            scheduleService.addSchedule(weekToDoFriendFalse);
+            scheduleService.addSchedule(weekHabitFriend);
+
+            ScheduleDto dayToDoPublicTrue = ScheduleDto.createForToDoSchedule(userA, "dayToDoPublicTrue", OpenScope.PUBLIC, dailyPeriod, true);
+            ScheduleDto dayToDoPublicFalse = ScheduleDto.createForToDoSchedule(userA, "dayToDoPublicFalse", OpenScope.PUBLIC, dailyPeriod, false);
+            ScheduleDto dayHabitPublic = ScheduleDto.createForHabitSchedule(userA, "dayHabitPublic", OpenScope.PUBLIC, dailyPeriod);
+            ScheduleDto dayToDoPrivateTrue = ScheduleDto.createForToDoSchedule(userA, "dayToDoPrivateTrue", OpenScope.PRIVATE, dailyPeriod, true);
+            ScheduleDto dayToDoPrivateFalse = ScheduleDto.createForToDoSchedule(userA, "dayToDoPublicTrue", OpenScope.PRIVATE, dailyPeriod, false);
+            ScheduleDto dayHabitPrivate = ScheduleDto.createForHabitSchedule(userA, "dayHabitPrivate", OpenScope.PRIVATE, dailyPeriod);
+            ScheduleDto dayToDoFriendTrue = ScheduleDto.createForToDoSchedule(userA, "dayToDoFriendTrue", OpenScope.FRIEND, dailyPeriod, true);
+            ScheduleDto dayToDoFriendFalse = ScheduleDto.createForToDoSchedule(userA, "dayToDoFriendTrue", OpenScope.FRIEND, dailyPeriod, false);
+            ScheduleDto dayHabitFriend = ScheduleDto.createForHabitSchedule(userA, "dayHabitFriend", OpenScope.FRIEND, dailyPeriod);
+
+            scheduleService.addSchedule(dayToDoPublicTrue);
+            scheduleService.addSchedule(dayToDoPublicFalse);
+            scheduleService.addSchedule(dayHabitPublic);
+            scheduleService.addSchedule(dayToDoPrivateTrue);
+            scheduleService.addSchedule(dayToDoPrivateFalse);
+            scheduleService.addSchedule(dayHabitPrivate);
+            scheduleService.addSchedule(dayToDoFriendTrue);
+            scheduleService.addSchedule(dayToDoFriendFalse);
+            scheduleService.addSchedule(dayHabitFriend);
+        }
+    }
 
     @Test
     public void 할일_월간_일정_추가() {
@@ -80,6 +172,7 @@ public class ScheduleServiceTest {
         Assertions.assertEquals(4, friendSchedule.getMonthly().getToDoScheduleList().size());
         Assertions.assertEquals(2, friendSchedule.getMonthly().getHabitScheduleList().size());
     }
+
     @Test
     public void 오늘_일정_전체_조회_주간() {
         AllCategorizedScheduleList publicSchedule1 = scheduleService.findAllCategorizedScheduleList(LocalDate.now(), "authIdA");
@@ -98,6 +191,7 @@ public class ScheduleServiceTest {
         Assertions.assertEquals(4, friendSchedule.getWeekly().getToDoScheduleList().size());
         Assertions.assertEquals(2, friendSchedule.getWeekly().getHabitScheduleList().size());
     }
+
     @Test
     public void 오늘_일정_전체_조회_일간() {
         AllCategorizedScheduleList publicSchedule1 = scheduleService.findAllCategorizedScheduleList(LocalDate.now(), "authIdA");
